9ff8928b87d29ff321543a8178cc77388a2c75fd
zeo911 <113481205+zeo911@users.noreply.github.com>
2024-04-07 04:32:49 +0200

    door.ino

--- door.ino
+++ door.ino
@@ -4,28 +4,28 @@
 #define LOCKED_POSITION 0
 #define UNLOCKED_POSITION 90
 
-const int servoPin = 2; // Pin connected to servo motor
+const int servoPin = 2; 
 Servo doorLockServo;
 
-const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Four columns
+const byte ROWS = 4;
+const byte COLS = 4;
 char keys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-byte rowPins[ROWS] = {5, 4, 3, 2}; // Connect to the row pinouts of the keypad
-byte colPins[COLS] = {9, 8, 7, 6}; // Connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {5, 4, 3, 2};
+byte colPins[COLS] = {9, 8, 7, 6};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-bool doorLocked = true; // Initial state is locked
-const String correctPin = "1234"; // Define your PIN code
+bool doorLocked = true;
+const String correctPin = "1234";
 
 void setup() {
   Serial.begin(9600);
   doorLockServo.attach(servoPin);
-  lockDoor(); // Ensure the door is locked on startup
+  lockDoor();
 }
 
 void loop() {
